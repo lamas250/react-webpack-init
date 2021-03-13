cc59c680daf30795b599a579dc676c5da15e1dfb
Igor <igorlamas1994@gmail.com>
2021-03-13 18:09:41 -0300

    Template Ts Sass

--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-import './styles/global.scss'
-
-export function App(){
-
-  return <h1>Helfflo</h1>
-}
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './styles/global.scss'
+
+export function App(){
+
+  return (<>
+    <h1>Template com Ts e Sass configurado</h1>
+  </>)
+}
